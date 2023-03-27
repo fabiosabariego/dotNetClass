2d131eb218681cda61d0b19a4b00ad89f4e2147f
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-27 07:36:03 -0300

    Upload File from PC

---
+++ BlazorFileUpload/Services/FileUpload.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorFileUpload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            this._logger = logger;
+            this._webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if(file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+                    using (var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
